4b6316ea624a552fe7fe23ceba61df0dfd697004
Aimable <niyoai@yahoo.fr>
2018-04-12 16:40:10 +0100

    adding some libraries

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Modal, View, Image, Text, Button, StyleSheet} from 'react-native';
+import { Modal, View, Image, Text, Button, StyleSheet, TouchableOpacity} from 'react-native';
 
-
+import Icon from 'react-native-vector-icons/Ionicons';
 
 
 const placeDetail = props => {
@@ -24,15 +24,25 @@ const placeDetail = props => {
             <View style={styles.modalContainer}>
                 {modalContent}
                 <View>
-                    <Button
-                        title="Delete"
-                        color="red"
-                        onPress={props.onItemDeleted}
-                    />
-                    <Button
-                        title="Close"
-                        onPress={props.onModalClosed}
-                    />
+                    <TouchableOpacity onPress={props.onItemDeleted}>
+                        <View style={styles.deleteButton}>
+                        <Icon
+                            size={50}
+                            name="ios-trash"
+                            color="red"
+                        />
+                        </View>
+                    </TouchableOpacity>
+                        {/*<Button*/}
+                            {/*title="Delete"*/}
+                            {/*color="red"*/}
+                            {/*onPress={props.onItemDeleted}*/}
+                        {/*/>*/}
+                        <Button
+                            title="Close"
+                            onPress={props.onModalClosed}
+                        />
+
                 </View>
             </View>
         </Modal>
@@ -51,6 +61,9 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         textAlign:"center",
         fontSize: 28
+    },
+    deleteButton: {
+        alignItems:"center"
     }
 });
 
